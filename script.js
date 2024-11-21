05e26804f1efa511821bd66f217797e5d449097d
Mj55500 <zhambaimerei@gmail.com>
2024-11-21 20:42:13 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function showInfo(text) {
-    const infoBox = document.getElementById("actor-info");
-    infoBox.textContent = text;
-}
-
-function hideInfo() {
-    const infoBox = document.getElementById("actor-info");
-    infoBox.textContent = "Наведите курсор на фото, чтобы увидеть описание.";
-}
